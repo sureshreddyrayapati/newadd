778ec6cc50b8ee90ce5cefc15984fc9ac0323ff2
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-27 17:10:09 +0530

    First

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace myconsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
